b86215f350b726904e44eb566a83f117116ae4f7
BeatsuDev <ericbieszczad@gmail.com>
2020-01-06 01:50:15 +0100

    File submitting + Form value extraction

--- core/cogs/replay_submission.py
+++ core/cogs/replay_submission.py
@@ -20,13 +20,18 @@ Description:
 
     @commands.command()
     async def form(self, ctx, *args):
+        # If the bot is already waiting for the user to send the form; return
+        if ctx.author.id in self.waiting_for: return
         if len(args) > 0:
             if args[0].lower()=='submit':
-                await self.submit(ctx)
+                await ctx.send('Send your form here or in DMs!')
+                await self._submit(ctx)
                 return
 
+        # Assuming there are no args; should just tell the user how to send a form
+
         embed = discord.Embed(
-            title=f'To submit a form, use the command `{self.bot.command_prefix(self.bot, ctx.message)}form submit`',
+            title=f'To submit a form, use the command `{self.bot.command_prefix(self.bot, ctx.message)[-1]}form submit`',
             colour=0xffff00)
 
         embed.set_author(
@@ -37,16 +42,84 @@ Description:
             icon_url='https://www.rtk-international.biz/images/Picto/RTK_Pictos_FormulairesServices.png',
             text='Copy paste the form below')
 
-        await ctx.channel.send(embed=embed)
-        await ctx.channel.send(self.FORM)
+        await ctx.send(embed=embed)
+        await ctx.send(self.FORM)
 
-    async def submit(ctx):
-        # Wait for form string message
-        # Retrieve form data
-        # Wait for replay files
-        # Store in DataBase
+# -----------------------------
+# Functions to used in the commands above
+# -----------------------------
+
+    def _from_format(self, string):
+        regexps = [
+            re.compile(r"^In-game name:(?P<in_game>.*)$"),
+            re.compile(r"^Twitch name:(?P<twitch_name>.+)$"),
+            re.compile(r"^Region:(?P<region>.+)$"),
+            re.compile(r"^Description:(?P<description>.+)$")
+        ]
+
+        matches = []
+
+        lines = string.split('\n')
+
+        for line in lines:
+            line = line.lstrip(' ')
+            line = line.strip()
+            for expression in regexps:
+                result = expression.search(line)
+                if result is not None:
+                    matches.append(result)
+
+        return [m.group(1).strip() for m in matches]
+
+
+    async def _submit(self, ctx):
+        check = lambda m: m.author.id == ctx.author.id
+        try:
+            self.waiting_for.append(m.author.id)
+            form = await self.bot.wait_for('message', check=check, timeout=60)
+        except asyncio.TimeoutError:
+            self.waiting_for.remove(ctx.member.id)
+            await ctx.send('Timed out')
+            return
+
+        try:
+            form = self._from_format(form.content)
+        except ValueError as e:
+            print(e)
+            await ctx.send("Couldn't retrieve data from the provided form! Make sure you have copy pasted the form!")
+            return
+
+        await ctx.send("Now for the files! Please send both replay files in one message!")
+        replay1, replay2 = await self._retrieve_replays(ctx, form)
+        await ctx.send(f"Players in the first replay: {[p.name for p in replay1.players]}")
+
+        self.waiting_for.remove(ctx.author.id)
+
+
+    async def _retrieve_replays(self, ctx, form, tempdir='replays/'):
+        replays = []
+        check = lambda m: m.author.id == ctx.author.id
+        while not len(replays) == 2:
+            try:
+                rp = await self.bot.wait_for('message', check=check, timeout=45)
+            except asyncio.TimeoutError:
+                self.waiting_for.remove(ctx.member.id)
+                await ctx.send('Timed out')
+                return
+
+            for f in rp.attachments:
+                if f.filename.endswith('.replay'):
+                    await f.save(tempdir+f.filename)
+                    rid = self.bot.bc.upload(tempdir+f.filename)[1]
+                    # Give ballchasing 60 seconds to process the replay
+                    await ctx.send('Give us 60 seconds for the replay to process')
+                    await asyncio.sleep(60)
+                    await ctx.send('Go ahead and send the next replay! (unless you have sent 2 already)')
+                    replays.append(self.bot.bc.replay(rid))
+                    os.remove(tempdir+f.filename)
+        return replays
 
 
 def setup(bot):
     bot.add_cog(ReplaySubmission(bot))
-    print("[COG] Added cog ReplaySubmission")
+    print("[COG] Added the cog ReplaySubmission")
