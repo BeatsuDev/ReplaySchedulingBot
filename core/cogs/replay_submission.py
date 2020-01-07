852bd7b43e59651667c7210c10fb62e3a89ec4c6
BeatsuDev <ericbieszczad@gmail.com>
2020-01-07 22:19:13 +0100

    Background processing for replay submissions + more commenting

--- core/cogs/replay_submission.py
+++ core/cogs/replay_submission.py
@@ -1,4 +1,4 @@
-import os, re, asyncio
+import os, re, time, asyncio
 
 import discord
 from discord.ext import commands
@@ -24,7 +24,8 @@ Description:
         if ctx.author.id in self.waiting_for: return
         if len(args) > 0:
             if args[0].lower()=='submit':
-                await ctx.send('Send your form here or in DMs!')
+                if not type(ctx.channel) == discord.DMChannel: return
+                await ctx.send('Send your form in DMs!')
                 await self._submit(ctx)
                 return
 
@@ -50,6 +51,7 @@ Description:
 # -----------------------------
 
     def _from_format(self, string):
+        """Get data from form formatted string"""
         regexps = [
             re.compile(r"^In-game name:(?P<in_game>.*)$"),
             re.compile(r"^Twitch name:(?P<twitch_name>.+)$"),
@@ -69,13 +71,16 @@ Description:
                 if result is not None:
                     matches.append(result)
 
+        # Return a list; length should be the same as the length of regexps
+        # The list should contain [in_game, twitch_name, region, description] in that order
         return [m.group(1).strip() for m in matches]
 
 
     async def _submit(self, ctx):
+        """Submit command"""
         check = lambda m: m.author.id == ctx.author.id
         try:
-            self.waiting_for.append(m.author.id)
+            self.waiting_for.append(ctx.author.id)
             form = await self.bot.wait_for('message', check=check, timeout=60)
         except asyncio.TimeoutError:
             self.waiting_for.remove(ctx.member.id)
@@ -90,16 +95,25 @@ Description:
             return
 
         await ctx.send("Now for the files! Please send both replay files in one message!")
-        replay1, replay2 = await self._retrieve_replays(ctx, form)
-        await ctx.send(f"Players in the first replay: {[p.name for p in replay1.players]}")
+        replays = await self._retrieve_replays(ctx)
+        valid, error = await self._check_replays(replays, ctx, form)
 
         self.waiting_for.remove(ctx.author.id)
+        if valid:
+            self._store_replays(self, replays)
+            await ctx.author.send('Your replays have successfully been stored!')
+            return
+
+        else:
+            await ctx.author.send(f"Your replay wasn't accepted for the following reason: {error}")
+            return
 
 
-    async def _retrieve_replays(self, ctx, form, tempdir='replays/'):
-        replays = []
-        check = lambda m: m.author.id == ctx.author.id
-        while not len(replays) == 2:
+    async def _retrieve_replays(self, ctx, tempdir='replays/'):
+        """Waits for user to send 2 attachments that end with .replay"""
+        tasks = []
+        check = lambda m: m.author.id == ctx.author.id and len(m.attachments)>0
+        while not len(tasks) == 2:
             try:
                 rp = await self.bot.wait_for('message', check=check, timeout=45)
             except asyncio.TimeoutError:
@@ -109,15 +123,50 @@ Description:
 
             for f in rp.attachments:
                 if f.filename.endswith('.replay'):
-                    await f.save(tempdir+f.filename)
-                    rid = self.bot.bc.upload(tempdir+f.filename)[1]
-                    # Give ballchasing 60 seconds to process the replay
-                    await ctx.send('Give us 60 seconds for the replay to process')
-                    await asyncio.sleep(60)
-                    await ctx.send('Go ahead and send the next replay! (unless you have sent 2 already)')
-                    replays.append(self.bot.bc.replay(rid))
-                    os.remove(tempdir+f.filename)
+                    # User has sent a .replay file: Process it here
+                    print(f'tempdir value: {tempdir}')
+                    dir = tempdir+f.filename
+
+                    await f.save(dir)
+                    rid = self.bot.bc.upload(dir)[1]
+                    os.remove(dir)
+
+                    downloadtask = self.bot.loop.create_task(self._ready_replay(rid))
+                    tasks.append(downloadtask)
+
+        # Wait for every task to complete
+        for task in tasks:
+            try:
+                asyncio.wait_for(task, timeout=90)
+            except asyncio.TimeoutError:
+                await ctx.send(f'Failed to process within 90 seconds. Form submission cancelled! Please try again <@{ctx.author.id}>')
+
+        # Tasks should be complete, so time to retrieve the results
+        replays = [task.result() for task in tasks]
         return replays
+
+
+    async def _ready_replay(self, id, interval=5):
+        """Attempts to load a replay repeatedly until succesfull or after 120 seconds has gone.
+        (Meant for giving ballchasing.com time to process the replay) In the future this should
+        be error handled within Ballchasing.replay() directly."""
+        ready = False
+        start = time.time()
+        while not ready:
+            if time.time()-start>120:
+                raise asyncio.TimeoutError
+            try:
+                replay = self.bot.bc.replay(id)
+                ready = True
+            except KeyError:
+                await asyncio.sleep(interval)
+        return replay
+
+
+    async def _check_replays(self, replays, ctx, form):
+        # Function to check if the user is allowed to upload; if the replays aren't ff's etc
+        # Should return (bool <valid>, str <error>)
+        pass
 
 
 def setup(bot):
