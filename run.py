18cfa619ba2313439c10ee375fb0f0252228dc10
BeatsuDev <ericbieszczad@gmail.com>
2020-05-21 19:30:31 +0200

    Added option to add own token

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 from core.bot import Turk
 
 if __name__ == '__main__':
-    token = os.environ.get('TOKEN')
+    token = os.environ.get('TOKEN', '<-- insert token -->')
     bot = Turk()
     bot.run(token)
